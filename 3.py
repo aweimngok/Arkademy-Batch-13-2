1f5ff2d8869387246b12149f9d919e6f48558e2c
aweimngok <39507571+aweimngok@users.noreply.github.com>
2019-11-02 22:01:19 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,22 +1,25 @@
+#Program by Fadhil Aweim
 import re
 import random
+#Deklarasi Fungsi
 def randomize(jumlah):
     bilangan = []
     total = 0
     for i in range (jumlah):
-        bilangan.append(random.randrange(1, 10, 2))
-        total += bilangan[i]
+        bilangan.append(random.randrange(1, 10, 2)) # Menambahkan data random yang telah di generate ke variable list (array) bilangan
+        total += bilangan[i] #Syntax untuk summing (increassing)
     print ("array : "+ str(bilangan))
     print ("sum : "+ str(total))
-    
+
+#Program Utama    
 angka = input ("Masukkan Angka : ")
 adaangka=0
 cari = re.findall("[0-9]", angka)
 for i in cari:
     adaangka+=1
-if adaangka>0:
+if adaangka>0: #Jika angka maka jalankan fungsi
     randomize(int(angka))
-else :
+else : #Jika Bukan angka
     print ("Input Bukan Angka")
 
 
